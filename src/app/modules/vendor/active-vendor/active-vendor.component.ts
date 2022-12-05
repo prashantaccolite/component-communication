207a1237a39661d5354c33f4eccc530e3a4f1745
Laura Rintoul <laura.rintoul@accolitedigital.com>
2022-12-05 09:29:33 +0000

    Showing of data, with button that prints vendor

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./active-vendor.component.scss']
 })
 export class ActiveVendorComponent implements OnInit {
-
+  vendors:any;
   constructor() { }
 
+  sendVendor(vendorObject:any) {
+    console.log(vendorObject);
+  }
+
   ngOnInit(): void {
+    this.vendors = [
+      {
+        "name": "vend1",
+        "age": 40,
+        "city": "city1",
+      },
+      {
+      "name": "vend2",
+      "age": 41,
+      "city": "city2",
+      },
+      {
+        "name": "vend3",
+        "age": 43,
+        "city": "city3",
+      },
+      {
+        "name": "vend4",
+        "age": 39,
+        "city": "city4",
+      },
+      {
+        "name": "vend5",
+        "age": 37,
+        "city": "city5",
+      },
+  ];
   }
 
 }
