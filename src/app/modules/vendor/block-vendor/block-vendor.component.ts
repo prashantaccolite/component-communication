65f803f15bb68a9802b7107c0bcd3e99aba87183
Stuti Sharma <stuti.sharma@accolite.com>
2022-12-06 06:25:04 +0530

    Solved Component Communication

--- src/app/modules/vendor/block-vendor/block-vendor.component.ts
+++ src/app/modules/vendor/block-vendor/block-vendor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Vendor } from 'src/app/vendor';
+import { VendorDataService } from 'src/app/vendor-data.service';
+
 
 @Component({
   selector: 'app-block-vendor',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockVendorComponent implements OnInit {
 
-  constructor() { }
+  blockedVendors:Vendor[] = [];
+  // addBlockedUserToArray(blockedVendor:Vendor){
+  //   this.blockedVendors.push(blockedVendor);
+  // }
+
+  constructor( private vendorService : VendorDataService) { }
 
   ngOnInit(): void {
+    this.vendorService.vendorObservable.subscribe((vendor) =>{
+      const checkIfPresent=this.blockedVendors.some((item)=>item.name===vendor.name)
+      if(checkIfPresent)
+      alert("already blocked")
+      else
+      this.blockedVendors.push(vendor);
+    }
+    )
   }
 
 }
